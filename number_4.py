7f784e78f0c78459290e8ccd11e0823521810f1c
JohnChristianIdul <idulchristian@gmail.com>
2023-09-04 14:59:55 +0800

    Final answer

--- number_4.py
+++ number_4.py
@@ -1,78 +1,18 @@
-# Write the following functions
-def decToBinary(dec):
-    if dec == 0:
-        return "0"
+# count number of digit in given integer
+a = int(input("Please enter a number: "))
+smallest = 100;
+highest = 0;
+count = 0;
+while(a!=0):
+    b = a%10
+    if(b <= smallest):
+        smallest = b;
+    if(b >= highest):
+        highest = b;
+    count+=1
+    a//=10;
 
-    binary = ""
-    while dec > 0:
-        remainder = dec % 2
-        binary = str(remainder) + binary
-        dec = dec // 2
+print("Number of digits in a given number is: ", count)
+print("Smallest number is: ", smallest)
+print("Highest number is: ", highest)
 
-    return binary
-
-def binaryToN(bin, type):
-    decimal = 0
-    power = 0
-
-    for bit in reversed(str(bin)):
-        if bit == '1':
-            decimal += 2 ** power
-        power += 1
-        print("bit: ", bit)
-        print("power: ",power)
-        print("decimal: ",decimal)
-
-    if type == "dec":
-        return decimal
-    elif type == "oct":
-        return decToOctal(decimal)
-    elif type == "hex":
-        return decToHex(decimal)
-
-
-def decToOctal(dec):
-    if dec == 0:
-        return "0"
-
-    octal = ""
-    while dec > 0:
-        remainder = dec % 8
-        octal = str(remainder) + octal
-        dec = dec // 8
-
-    return octal
-
-def decToHex(dec):
-    if dec == 0:
-        return "0"
-
-    hex = ""
-    while dec > 0:
-        remainder = dec % 16
-        hex = str(remainder) + hex
-        dec = dec // 16
-
-    return hex
-
-def main():
-    decimal_number = int(input("Enter a decimal number: "))
-    b = decToBinary(decimal_number)
-    o = decToOctal(decimal_number)
-    h = decToHex(decimal_number)
-    print("The binary representation of", decimal_number, " is ", b)
-    print("The octal representation of", decimal_number, " is ", o)
-    print("The hexadecimal representation of", decimal_number, " is ", h)
-
-    binary_number = int(input("Enter a binary number: "))
-    t = input("Convert binary to what type (dec/oct/hex): ")
-    n = binaryToN(binary_number, t)
-
-    if t == "dec":
-        print("The decimal representation of", binary_number, " is ", n)
-    elif t == "oct":
-        print("The octal representation of", binary_number, " is ", n)
-    elif t == "hex":
-        print("The hexadecimal representation of", binary_number, " is ", n)
-
-main()
---
+++ number_5.py
@@ -0,0 +1,78 @@
+# Write the following functions
+def decToBinary(dec):
+    if dec == 0:
+        return "0"
+
+    binary = ""
+    while dec > 0:
+        remainder = dec % 2
+        binary = str(remainder) + binary
+        dec = dec // 2
+
+    return binary
+
+def binaryToN(bin, type):
+    decimal = 0
+    power = 0
+
+    for bit in reversed(str(bin)):
+        if bit == '1':
+            decimal += 2 ** power
+        power += 1
+        print("bit: ", bit)
+        print("power: ",power)
+        print("decimal: ",decimal)
+
+    if type == "dec":
+        return decimal
+    elif type == "oct":
+        return decToOctal(decimal)
+    elif type == "hex":
+        return decToHex(decimal)
+
+
+def decToOctal(dec):
+    if dec == 0:
+        return "0"
+
+    octal = ""
+    while dec > 0:
+        remainder = dec % 8
+        octal = str(remainder) + octal
+        dec = dec // 8
+
+    return octal
+
+def decToHex(dec):
+    if dec == 0:
+        return "0"
+
+    hex = ""
+    while dec > 0:
+        remainder = dec % 16
+        hex = str(remainder) + hex
+        dec = dec // 16
+
+    return hex
+
+def main():
+    decimal_number = int(input("Enter a decimal number: "))
+    b = decToBinary(decimal_number)
+    o = decToOctal(decimal_number)
+    h = decToHex(decimal_number)
+    print("The binary representation of", decimal_number, " is ", b)
+    print("The octal representation of", decimal_number, " is ", o)
+    print("The hexadecimal representation of", decimal_number, " is ", h)
+
+    binary_number = int(input("Enter a binary number: "))
+    t = input("Convert binary to what type (dec/oct/hex): ")
+    n = binaryToN(binary_number, t)
+
+    if t == "dec":
+        print("The decimal representation of", binary_number, " is ", n)
+    elif t == "oct":
+        print("The octal representation of", binary_number, " is ", n)
+    elif t == "hex":
+        print("The hexadecimal representation of", binary_number, " is ", n)
+
+main()
